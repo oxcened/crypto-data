a3a204192b428fef116c0628f1567881d52f0ebe
Alen Ajam <alen.ajam@live.com>
2023-06-06 21:45:08 +0200

    feat: add footer

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,29 @@ export default function App() {
   const { data, isLoading, isError, fetch, sortRecentTrades, sort } = useData();
 
   return (
-    <main className="px-4 py-6 lg:py-10 max-w-md mx-auto">
-      <h1 className="text-4xl text-center">Welcome to CryptoData 👋</h1>
-      <p className="text-xl mt-3 text-center">
-        Please select a pair and hit the button.
-      </p>
+    <>
+      <main className="px-4 py-6 lg:py-10 max-w-md mx-auto">
+        <h1 className="text-4xl text-center">Welcome to CryptoData 👋</h1>
+        <p className="text-xl mt-3 text-center">
+          Please select a pair and hit the button.
+        </p>
 
-      <Form isLoading={isLoading} onSubmit={fetch} />
+        <Form isLoading={isLoading} onSubmit={fetch} />
 
-      {isLoading && 'Loading, please wait...'}
-      {isError && 'Ouch, an error occurred! Please try again.'}
-      {data && (
-        <PairData
-          data={data}
-          sort={sort}
-          onSortRecentTrades={sortRecentTrades}
-        />
-      )}
-    </main>
+        {isLoading && 'Loading, please wait...'}
+        {isError && 'Ouch, an error occurred! Please try again.'}
+        {data && (
+          <PairData
+            data={data}
+            sort={sort}
+            onSortRecentTrades={sortRecentTrades}
+          />
+        )}
+      </main>
+
+      <footer className="text-center pb-10 text-sm">
+        Made with ❤️ by Alen
+      </footer>
+    </>
   );
 }
