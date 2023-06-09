360b38d00da1f6d870a60263255167f80372a347
Alen Ajam <alen.ajam@live.com>
2023-06-09 21:27:00 +0200

    fix: minimumFractionDigits in formatters

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -1,8 +1,10 @@
 export const priceFormatter = new Intl.NumberFormat('en-US', {
   maximumFractionDigits: 2,
+  minimumFractionDigits: 2
 });
 
 export const percentFormatter = new Intl.NumberFormat('en-US', {
   style: 'percent',
   maximumFractionDigits: 2,
+  minimumFractionDigits: 2
 });
